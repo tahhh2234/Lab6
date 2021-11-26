d68c8022e3286550f16655edb64aeec1e050200f
tahhh <thadpongsermsook1@gmail.com>
2021-11-26 16:29:37 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,35 +2,29 @@
 using namespace std;
 
 int main(){
-    int x;
-    int z;
-    int num[x];
-    while(num[x] != 0){
+    int num, even = -1, odd = 0;;
+    cout << "Enter an integer: ";
+    cin >> num;
+        if(num%2 == 0){
+            even++;
+        }else{
+            odd++;
+        }
+        
+    while(num != 0){
         cout << "Enter an integer: ";
-        cin >> num[x];
-        z++;
+        cin >> num;
+        if(num%2 == 0){
+            even++;
+        }else{
+            odd++;
+        }
+        
     }
 
     cout << "#Even numbers = ";
-    int even = -1;
-    int i = 0;
-    while(i < z){
-        if(num[i]%2 == 0){
-            even++;
-        }
-        i++;
-    }
     cout << even << endl;
-
     cout << "#Odd numbers = ";
-    int odd = 0;
-    int y = 0;
-    while(y < z){
-        if(num[y]%2 != 0){
-            odd++;
-        }
-        y++;
-    }
     cout << odd;
 
     return 0;
